38d741f97d7435d96cbda2045aeb671512859ccb
LIPUN <LIPUN@DeviPrasad-PC>
2024-05-30 23:25:16 +0530

    extends BaseTest

--- com.ShoppersStack_M4/src/test/java/Students/DeviPrasad.java
+++ com.ShoppersStack_M4/src/test/java/Students/DeviPrasad.java
@@ -1,5 +1,9 @@
 package Students;
 
-public class DeviPrasad {
+import com.ShoppersStack.GenericUtility.BaseTest;
 
+public class DeviPrasad extends BaseTest {
+	public static void main(String[] args) {
+		
+	}
 }
